d0fdfe720dfd407e141bd01d955d9d111ade111c
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-25 17:21:15 +0500

    feat: specify ErrorHandler interface properties

--- stable_map/handler.py
+++ stable_map/handler.py
@@ -1,8 +1,9 @@
 import abc
-from typing import Generic
+from typing import Generic, Sequence
 
 from stable_map.context import ExceptionType, T
 
 
 class ErrorHandler(abc.ABC, Generic[T, ExceptionType]):
-    ...
+    __exceptions: Sequence[type[ExceptionType]]
+    __ignore: Sequence[type[ExceptionType]]
