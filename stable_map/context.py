d7300e3d763043ee854e026bf0ab00e2360b962c
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-25 17:15:00 +0500

    feat: ErrorContext DTO class

--- stable_map/context.py
+++ stable_map/context.py
@@ -1,4 +1,12 @@
-from typing import TypeVar
+from dataclasses import dataclass
+from typing import TypeVar, Generic
 
 ExceptionType = TypeVar('ExceptionType', bound=Exception)
 T = TypeVar('T')
+
+
+@dataclass
+class ErrorContext(Generic[T, ExceptionType]):
+    index: int
+    element: T
+    exception: ExceptionType
