ab8bda5dca38c806c7e32dbca6c34b7c6a6aa682
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-25 18:41:56 +0500

    feat: PickleHandler init method

--- stable_map/handlers/storage.py
+++ stable_map/handlers/storage.py
@@ -1,9 +1,16 @@
 import pickle
 from pathlib import Path
-from typing import Any, Callable
+from typing import Any, Callable, Sequence
 
 from stable_map.context import ErrorContext
 from stable_map.handler import ErrorHandler
+
+
+def __get_default_file_name(context: ErrorContext[Any, Exception]) -> str:
+    exc_type = type(context.exception)
+    index = context.index
+
+    return f'{exc_type}_{index}'
 
 
 class PickleHandler(ErrorHandler[Any, Exception]):
@@ -12,3 +19,16 @@ class PickleHandler(ErrorHandler[Any, Exception]):
     __file_name: str | Path | Callable[
         [ErrorContext[Any, Exception]], str | Path
     ]
+
+    def __init__(
+        self,
+        exceptions: Sequence[type[Exception]],
+        ignore: Sequence[type[Exception]],
+        dest: Path,
+        file_name: str | Path | Callable[
+            [ErrorContext[Any, Exception]], str | Path
+        ] = __get_default_file_name,
+    ) -> None:
+        super().__init__(exceptions, ignore)
+        self.__dest = dest
+        self.__file_name = file_name
