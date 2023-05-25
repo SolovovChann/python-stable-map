29f99eb5a7a38a5840baa1a85306f6da7fdfd18c
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-25 17:35:29 +0500

    feat: specify StableMap properties

--- stable_map/stablemap.py
+++ stable_map/stablemap.py
@@ -1,9 +1,13 @@
-from typing import Generic, TypeVar
+from typing import Callable, Generic, Iterable, Sequence, TypeVar
 
-from stable_map.context import T
+from stable_map.context import ErrorContext, T
+from stable_map.handler import ErrorHandler
 
 S = TypeVar('S')
 
 
 class StableMap(Generic[T, S]):
-    ...
+    __default: S | Callable[[ErrorContext[T, Exception]], S] | None
+    __function: Callable[[T], S]
+    __handlers: Sequence[ErrorHandler[T, Exception]]
+    __sequence: Iterable[T]
