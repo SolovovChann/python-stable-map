4afefe38b48ce6172b2042f4ebb1e4ca76d1b1bf
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-25 18:01:39 +0500

    docs: RaiseExceptionHandler docstring

--- stable_map/handlers/exceptions.py
+++ stable_map/handlers/exceptions.py
@@ -5,5 +5,10 @@ from stable_map.handler import ErrorHandler
 
 
 class RaiseExceptionHandler(ErrorHandler[Any, Exception]):
+    """
+    Add this handler to the `StableMap` to stop it's processing
+    if any exception occurred.
+    """
+
     def handle(self, context: ErrorContext[Any, Exception]) -> None:
         raise context.exception
