79a72d437a5cce8e9879ce7af3399dc13d79ef00
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-25 17:21:42 +0500

    feat: ErrorHandler handle method

--- stable_map/handler.py
+++ stable_map/handler.py
@@ -1,9 +1,13 @@
 import abc
 from typing import Generic, Sequence
 
-from stable_map.context import ExceptionType, T
+from stable_map.context import ErrorContext, ExceptionType, T
 
 
 class ErrorHandler(abc.ABC, Generic[T, ExceptionType]):
     __exceptions: Sequence[type[ExceptionType]]
     __ignore: Sequence[type[ExceptionType]]
+
+    @abc.abstractmethod
+    def handle(self, context: ErrorContext[T, ExceptionType]) -> None:
+        ...
