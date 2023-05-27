ca13bedd06d50dc5dcb46930b1911fc34100c8a4
SolovovChan <solnik2001@gmail.com>
2023-05-27 15:50:25 +0500

    feat: import default classes at init

--- stable_map/__init__.py
+++ stable_map/__init__.py
@@ -1,6 +1,11 @@
 """
-A tool similar to the built-in python map, 
+A tool similar to the built-in python map,
 but reacting to exceptions that occur
 """
 
+from .context import ErrorContext
+from .handler import ErrorHandler
+from .stablemap import StableMap
+
+__all__ = 'StableMap', 'ErrorHandler', 'ErrorContext'
 __version__ = '0.1.0'
