a32dd2ac6e25af6d822cdbe918246de06350473e
SolovovChan <solnik2001@gmail.com>
2023-05-29 12:41:33 +0500

    chore: set the default exceptions

--- stable_map/handlers/info.py
+++ stable_map/handlers/info.py
@@ -12,8 +12,8 @@ class LoggingHandler(ErrorHandler[Any, Exception]):
 
     def __init__(
         self,
-        exceptions: Sequence[type[Exception]],
-        ignore: Sequence[type[Exception]],
+        exceptions: Sequence[type[Exception]] = [Exception],
+        ignore: Sequence[type[Exception]] = [],
         logger: logging.Logger | None = None,
     ) -> None:
         if logger is None:
