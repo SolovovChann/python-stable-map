647a773910ea3c5ea8f5d26baf6793a251542242
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-30 14:25:51 +0500

    test: StableMap iteration unit test

--- tests/test_stablemap.py
+++ tests/test_stablemap.py
@@ -1,8 +1,10 @@
 import unittest
 from typing import Any
+from unittest import mock
 
 from stable_map.context import ErrorContext
 from stable_map.handler import ErrorHandler
+from stable_map.stablemap import StableMap
 
 
 class ErrorHandlerStub(ErrorHandler[Any, Exception]):
@@ -15,4 +17,14 @@ def divide_100_to(divider: float) -> float:
 
 
 class StableMapTest(unittest.TestCase):
-    ...
+    def test_iteration(self) -> None:
+        sequence = [3, 4, 1, 2, 0, 5, 8]
+        reference = [33, 25, 100, 50, 20, 12]
+
+        handler = ErrorHandlerStub([Exception], [])
+        handler.handle = mock.MagicMock()
+
+        stable_map = StableMap(divide_100_to, sequence, [handler])
+
+        for value, ref in zip(stable_map, reference):
+            self.assertEqual(value, ref)
