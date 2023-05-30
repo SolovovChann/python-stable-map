c7412497248fdd0fffefcb06f86269adc7674e8e
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2023-05-30 14:24:02 +0500

    feat: callback function

--- tests/test_stablemap.py
+++ tests/test_stablemap.py
@@ -7,3 +7,7 @@ from stable_map.handler import ErrorHandler
 class ErrorHandlerStub(ErrorHandler[Any, Exception]):
     def handle(self, context: ErrorContext[Any, Exception]) -> None:
         pass
+
+
+def divide_100_to(divider: float) -> float:
+    return 100 // divider
