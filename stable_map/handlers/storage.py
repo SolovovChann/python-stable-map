4b99ee50df26a52ccf1cf1664145f16aa6cba1be
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2024-07-19 16:33:35 +0500

    chore: inherit PickleDumpHandler from Storage

--- stable_map/handlers/storage.py
+++ stable_map/handlers/storage.py
@@ -77,56 +77,8 @@ class Storage(ErrorHandler[T, ExceptionType]):
         return self.__filename
 
 
-def _get_default_file_name(context: ErrorContext[Any, Exception]) -> str:
-    element_type = type(context.element).__name__.lower()
-    index = context.index
+class PickleDumpHandler(Storage[object, Exception]):
+    """Save failed elements as pickle files"""
 
-    return f'{element_type}_{index}.pickle'
-
-
-class PickleDumpHandler(ErrorHandler[Any, Exception]):
-    __context: ErrorContext[Any, Exception]
-    __dest: Path
-    __file_name: str | Path | Callable[
-        [ErrorContext[Any, Exception]], str | Path
-    ]
-
-    def __init__(
-        self,
-        dest: Path | str,
-        exceptions: Sequence[type[Exception]] = [Exception],
-        ignore: Sequence[type[Exception]] = [],
-        file_name: str | Path | Callable[
-            [ErrorContext[Any, Exception]], str | Path
-        ] = _get_default_file_name,
-    ) -> None:
-        if isinstance(dest, str):
-            dest = Path(dest)
-
-        super().__init__(exceptions, ignore)
-        self.__dest = dest
-        self.__file_name = file_name
-
-    def handle(self, context: ErrorContext[Any, Exception]) -> None:
-        self.__context = context
-        output_file = self.__dest / self.__eval_file_name()
-        data = self.__dump_data()
-
-        output_file.write_bytes(data)
-
-    def __eval_file_name(self) -> Path:
-        if callable(self.__file_name):
-            file_name = self.__file_name(self.__context)
-        else:
-            file_name = self.__file_name
-
-        if isinstance(file_name, str):
-            file_name = Path(file_name)
-
-        return file_name
-
-    def __dump_data(self) -> Any:
-        element = self.__context.element
-        data = pickle.dumps(element)
-
-        return data
+    def _encode_content(self, element: object) -> ByteString:
+        return pickle.dumps(element)
