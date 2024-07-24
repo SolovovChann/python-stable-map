a4ed6888064030fbeef358ef65520377551716dc
SolovovChann <60061582+SolovovChann@users.noreply.github.com>
2024-07-24 17:15:44 +0500

    feat: BufferWriterTest test handle method

--- tests/handlers/test_buffer_writer.py
+++ tests/handlers/test_buffer_writer.py
@@ -2,6 +2,7 @@ from io import StringIO
 from typing import Any, Callable
 from unittest.mock import Mock
 
+from stable_map.handler import ErrorContext
 from stable_map.handlers.info import ENCODER, BufferWriter
 
 from .test_error_handler import ErrorHandlerTest
@@ -18,3 +19,10 @@ class BufferWriterTest(ErrorHandlerTest):
         cls.handler = BufferWriter(cls.buffer, cls.encoding_callback)
         cls.exceptions = [Exception]
         cls.ignore = []
+
+    def test_handle(self) -> None:
+        context = ErrorContext(0, "test_element", Exception())
+
+        self.encoding_callback.return_value = "encoded_context"
+        self.handler.handle(context)
+        self.assertEqual(self.buffer.getvalue(), "encoded_context")
